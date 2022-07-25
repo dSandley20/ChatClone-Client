321c08726d604f0733ab48a6b98f232a216a9c98
dSandley20 <danielsandley2@gmail.com>
2022-07-25 11:48:07 -0400

    boilerplate for LoginForm

---
+++ src/renderer/components/Form/LoginForm/index.tsx
@@ -0,0 +1,10 @@
+import IUserLogin from 'interfaces/user/IUserLogin';
+import { Box, Button } from '@mui/material';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
+
+const LoginForm = () => {
+  return <></>;
+};
+
+export default LoginForm;
