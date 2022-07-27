97337ed7a6f75d55b7a0f7b99077f339513c4a22
dSandley20 <danielsandley2@gmail.com>
2022-07-27 15:19:29 -0400

    kitchen sink / test page

---
+++ src/renderer/pages/TestPage/index.tsx
@@ -0,0 +1,44 @@
+import LoginForm from 'renderer/components/Form/LoginForm';
+import CreateUserForm from 'renderer/components/Form/CreateUserForm';
+import icon from '../../../../assets/icon.svg';
+
+const TestPage = () => {
+  return (
+    <div>
+      <div className="Hello">
+        <img width="200px" alt="icon" src={icon} />
+      </div>
+      <h1>electron-react-boilerplate</h1>
+      <div className="Hello">
+        <a
+          href="https://electron-react-boilerplate.js.org/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <button type="button">
+            <span role="img" aria-label="books">
+              📚
+            </span>
+            Read our docs
+          </button>
+        </a>
+        <a
+          href="https://github.com/sponsors/electron-react-boilerplate"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <button type="button">
+            <span role="img" aria-label="books">
+              🙏
+            </span>
+            Donate
+          </button>
+        </a>
+        {/* <LoginForm /> */}
+        <CreateUserForm />
+      </div>
+    </div>
+  );
+};
+
+export default TestPage;
