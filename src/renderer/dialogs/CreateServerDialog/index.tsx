94e0e7674a5c79e8a6974c8bf5ebe4cb1494210c
dSandley20 <danielsandley2@gmail.com>
2022-07-28 17:59:06 -0400

    basic functionality for create server dialog

---
+++ src/renderer/dialogs/CreateServerDialog/index.tsx
@@ -0,0 +1,39 @@
+import { Dialog, DialogTitle, Button, TextField } from '@mui/material';
+import useCreateServer from 'api/server/create';
+import createServerValues from 'formik/server/create';
+import useFormikWithMaterialUI from 'hooks/useFormikWithMui';
+
+export interface DefaultDialogProps {
+  open: boolean;
+  onClose: (value: string) => void;
+}
+
+const CreateServerDialog = (props: DefaultDialogProps) => {
+  const mutation = useCreateServer();
+  const { values, schema, callback } = createServerValues(mutation);
+  const formik = useFormikWithMaterialUI(values, schema, callback);
+
+  const { open, onClose } = props;
+
+  return (
+    <>
+      <Dialog open={open} onClose={onClose}>
+        <DialogTitle>Create Server</DialogTitle>
+        <form onSubmit={formik.handleSubmit}>
+          <TextField
+            fullWidth
+            id="username"
+            name="username"
+            label="Username"
+            value={formik.values.name}
+            onChange={formik.handleChange}
+            error={formik.touched.name && Boolean(formik.errors.name)}
+          />
+          <Button>Create</Button>
+        </form>
+      </Dialog>
+    </>
+  );
+};
+
+export default CreateServerDialog;
