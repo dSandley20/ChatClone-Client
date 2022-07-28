8259d648c828bef4e486c1ac078fc13731bffd29
dSandley20 <danielsandley2@gmail.com>
2022-07-28 17:58:11 -0400

    started planning out routes

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,6 +1,7 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import TestPage from './pages/TestPage';
+import LoginPage from './pages/LoginPage';
 import './App.css';
 
 // opens the electron store to the app
@@ -18,11 +19,26 @@ declare global {
 export default function App() {
   const queryClient = new QueryClient();
 
+  // TODO create 404 page
+
   return (
     <QueryClientProvider client={queryClient}>
       <Router>
         <Routes>
-          <Route path="/" element={<TestPage />} />
+          <Route path="/" element={<TestPage />}>
+            <Route path="auth" element={<LoginPage />} />
+            {/* <Route path="/server">
+            <Route path=":serverId" />
+          </Route> */}
+          </Route>
+          <Route
+            path="*"
+            element={
+              <main style={{ padding: '1rem' }}>
+                <p>Hi! I think you are lost!</p>
+              </main>
+            }
+          />
         </Routes>
       </Router>
     </QueryClientProvider>
