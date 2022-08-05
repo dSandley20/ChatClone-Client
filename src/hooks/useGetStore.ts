2948a4e3583a12ca13a3fc7cb08c8657d7075879
dSandley20 <danielsandley2@gmail.com>
2022-08-05 11:25:44 -0400

    remove electron store hooks

--- src/hooks/useGetStore.ts
+++
@@ -1,3 +0,0 @@
-export default function useGetStore(key: string): any {
-  return window.electron.store.get(key);
-}
--- src/hooks/useSaveStore.ts
+++
@@ -1,3 +0,0 @@
-export default function useSaveStore(key: string, value: any): void {
-  window.electron.store.set(key, value);
-}
