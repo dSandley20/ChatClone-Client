38a139c62df6330831e7795b7798157a3822f7ac
dSandley20 <danielsandley2@gmail.com>
2022-08-05 10:40:35 -0400

    boiler plate for main display

--- src/renderer/pages/ServerPage/index.tsx
+++ src/renderer/pages/ServerPage/index.tsx
@@ -1,9 +1,33 @@
 import ServerMenu from 'renderer/components/ServerMenu';
+import { Box, Grid } from '@mui/material';
+import useListServer from 'api/server/list';
+import { useServerContext } from 'renderer/context/ServerContext';
+import LoadingFailed from 'renderer/components/LoadingFailed';
 
 const ServerPage = () => {
+  const { data, isSuccess, isError } = useListServer();
+  const { selectedServer, selectedServerId } = useServerContext();
+
   return (
     <>
-      <ServerMenu />
+      <Grid container spacing={1}>
+        <Grid item xs={3}>
+          <ServerMenu data={data} />
+        </Grid>
+        {isSuccess && selectedServerId === 0 && (
+          <Box> {/* TODO have message/server panel */}</Box>
+        )}
+        {isSuccess && selectedServerId > 0 && (
+          <Box> {/* TODO show last active server */}</Box>
+        )}
+        {isError && (
+          <Box>
+            <LoadingFailed />
+          </Box>
+        )}
+
+        <Grid item xs={9} />
+      </Grid>
     </>
   );
 };
