2f5507bff70619f8eb87b475ef24c870672a5748
dSandley20 <danielsandley2@gmail.com>
2022-08-05 10:31:32 -0400

    add contexts and test routes

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,20 +1,11 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import TestPage from './pages/TestPage';
+import { ServerProvider } from './context/ServerContext';
+import { AuthProvider } from './context/AuthContext';
 import LoginPage from './pages/LoginPage';
+import LoadingPage from './pages/LoadingPage';
+import ServerPage from './pages/ServerPage';
 import './App.css';
-
-// opens the electron store to the app
-declare global {
-  interface Window {
-    electron: {
-      store: {
-        get: (key: string) => any;
-        set: (key: string, val: any) => void;
-      };
-    };
-  }
-}
 
 export default function App() {
   const queryClient = new QueryClient();
@@ -23,24 +14,28 @@ export default function App() {
 
   return (
     <QueryClientProvider client={queryClient}>
-      <Router>
-        <Routes>
-          <Route path="/" element={<TestPage />}>
-            <Route path="auth" element={<LoginPage />} />
-            {/* <Route path="/server">
-            <Route path=":serverId" />
-          </Route> */}
-          </Route>
-          <Route
-            path="*"
-            element={
-              <main style={{ padding: '1rem' }}>
-                <p>Hi! I think you are lost!</p>
-              </main>
-            }
-          />
-        </Routes>
-      </Router>
+      <AuthProvider>
+        <ServerProvider>
+          {/* <ThemeProvider theme={theme}> */}
+          <Router>
+            <Routes>
+              <Route path="/" element={<LoadingPage />} />
+              <Route path="/auth" element={<LoginPage />} />
+              <Route path="/server" element={<ServerPage />} />
+
+              <Route
+                path="*"
+                element={
+                  <main style={{ padding: '1rem' }}>
+                    <p>Hi! I think you are lost!</p>
+                  </main>
+                }
+              />
+            </Routes>
+          </Router>
+          {/* </ThemeProvider> */}
+        </ServerProvider>
+      </AuthProvider>
     </QueryClientProvider>
   );
 }
