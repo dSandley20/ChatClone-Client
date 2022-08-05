ec813a1c1b56ec740eb49b6000fcf6433b64d7b8
dSandley20 <danielsandley2@gmail.com>
2022-08-05 10:39:05 -0400

    ServerMenu sidebar

--- src/renderer/components/ServerMenu/index.tsx
+++ src/renderer/components/ServerMenu/index.tsx
@@ -1,15 +1,62 @@
-import { Stack, Item } from '@mui/material';
+/* eslint-disable no-underscore-dangle */
+import { Stack } from '@mui/material';
+import IServerButton from 'interfaces/server/IServerButton';
+import { useState } from 'react';
+import CreateServerDialog from 'renderer/dialogs/CreateServerDialog';
+import IServer from 'interfaces/server/IServer';
+import { useServerContext } from 'renderer/context/ServerContext';
+import ServerButton from '../ServerButton';
 
-const ServerMenu = () => {
-  // TODO call list of servers for the user
+export interface IServerMenu {
+  data: IServer[];
+}
+
+const ServerMenu = (props: IServerMenu) => {
+  const { changeServer } = useServerContext();
+
+  const [isOpen, setIsOpen] = useState(false);
+  const { data } = props;
+
+  const createServerProps: IServerButton = {
+    serverName: 'create server',
+    serverInitials: '+',
+    onClick: () => {
+      setIsOpen(true);
+    },
+    onClose: () => {
+      setIsOpen(false);
+    },
+  };
 
   return (
     <>
       <Stack spacing={2}>
-        <Item>Item 1</Item>
-        <Item>Item 2</Item>
-        <Item>Item 3</Item>
+        {data.map((server) => {
+          let initials = '';
+          for (let index = 0; index < server.name.split('-').length; index++) {
+            initials += server.name.split('-')[index][0].toUpperCase();
+          }
+          return (
+            <ServerButton
+              key={server.name}
+              serverName={server.name}
+              serverInitials={initials}
+              onClick={() => {
+                changeServer(server.id);
+              }}
+              onClose={createServerProps.onClose}
+            />
+          );
+        })}
+        {/* Button to create a new server at the bottom of the stack */}
+        <ServerButton
+          serverName={createServerProps.serverName}
+          serverInitials={createServerProps.serverInitials}
+          onClick={createServerProps.onClick}
+          onClose={createServerProps.onClose}
+        />
       </Stack>
+      <CreateServerDialog open={isOpen} onClose={createServerProps.onClose} />
     </>
   );
 };
