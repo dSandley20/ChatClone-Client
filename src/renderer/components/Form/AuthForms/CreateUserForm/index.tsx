804c359b7ca87969057a3ded639099127ca53523
dSandley20 <danielsandley2@gmail.com>
2022-08-05 10:37:29 -0400

    Form to create/login user

---
+++ src/renderer/components/Form/AuthForms/CreateUserForm/index.tsx
@@ -0,0 +1,94 @@
+import { useEffect, useState } from 'react';
+import createValues from 'formik/auth/create';
+import useCreateUserHook from 'api/user/create';
+import useFormikWithMaterialUI from 'hooks/useFormikWithMui';
+import { TextField, Button, Snackbar } from '@mui/material';
+import { useServerContext } from 'renderer/context/ServerContext';
+import { AuthTextField, AuthButton } from '../styles';
+
+// TODO style
+const CreateUserForm = () => {
+  const { changeServer } = useServerContext();
+  const [isOpen, setIsOpen] = useState(false);
+  const [responseMessage, setResponseMessage] = useState('');
+
+  const mutation = useCreateUserHook();
+  const { values, schema, callback } = createValues(mutation);
+  const formik = useFormikWithMaterialUI(values, schema, callback);
+
+  const handleClose = (
+    event: React.SyntheticEvent | Event,
+    reason?: string
+  ) => {
+    if (reason === 'clickaway') return;
+    setIsOpen(false);
+  };
+
+  useEffect(() => {
+    if (mutation.isSuccess) {
+      setIsOpen(true);
+      changeServer(0);
+      setResponseMessage('Successfully Logged in');
+    }
+    if (mutation.isError) {
+      setIsOpen(true);
+      setResponseMessage('Error creating user');
+    }
+  }, [mutation.isSuccess, mutation.isError, changeServer]);
+
+  return (
+    <>
+      <div>
+        <form
+          style={{ display: 'inline-grid', justifyItems: 'center' }}
+          onSubmit={formik.handleSubmit}
+        >
+          <AuthTextField
+            fullWidth
+            id="username"
+            name="username"
+            label="Username"
+            value={formik.values.username}
+            onChange={formik.handleChange}
+            error={formik.touched.username && Boolean(formik.errors.username)}
+          />
+          <AuthTextField
+            fullWidth
+            id="email"
+            name="email"
+            label="Email"
+            value={formik.values.email}
+            onChange={formik.handleChange}
+            error={formik.touched.email && Boolean(formik.errors.email)}
+          />
+          <AuthTextField
+            fullWidth
+            id="password"
+            name="password"
+            label="Password"
+            type="password"
+            value={formik.values.password}
+            onChange={formik.handleChange}
+            error={formik.touched.password && Boolean(formik.errors.password)}
+          />
+          <AuthButton
+            color="primary"
+            variant="contained"
+            fullWidth
+            type="submit"
+          >
+            Submit
+          </AuthButton>
+        </form>
+        <Snackbar
+          open={isOpen}
+          autoHideDuration={6000}
+          onClose={handleClose}
+          message={responseMessage}
+        />
+      </div>
+    </>
+  );
+};
+
+export default CreateUserForm;
---
+++ src/renderer/components/Form/AuthForms/LoginForm/index.tsx
@@ -0,0 +1,85 @@
+import { Button, TextField, Snackbar } from '@mui/material';
+import useAuthHook from 'api/user/auth';
+import useFormikWithMaterialUI from 'hooks/useFormikWithMui';
+import authValues from 'formik/auth/auth';
+import { useEffect, useState } from 'react';
+import { useServerContext } from 'renderer/context/ServerContext';
+import { AuthTextField, AuthButton } from '../styles';
+
+// TODO - needs stlying
+const LoginForm = () => {
+  const { changeServer } = useServerContext();
+  const [isOpen, setIsOpen] = useState(false);
+  const [responseMessage, setResponseMessage] = useState('');
+
+  const mutation = useAuthHook();
+  const { values, schema, callback } = authValues(mutation);
+  const formik = useFormikWithMaterialUI(values, schema, callback);
+
+  useEffect(() => {
+    if (mutation.isSuccess) {
+      setIsOpen(true);
+      // TODO set last active server
+      setResponseMessage('Successfully Logged in');
+    }
+    if (mutation.isError) {
+      setIsOpen(true);
+      setResponseMessage('User not found');
+    }
+  }, [mutation.isSuccess, mutation.isError]);
+
+  const handleClose = (
+    event: React.SyntheticEvent | Event,
+    reason?: string
+  ) => {
+    if (reason === 'clickaway') return;
+    setIsOpen(false);
+  };
+
+  return (
+    <>
+      <div>
+        <form
+          style={{ display: 'inline-grid', justifyItems: 'center' }}
+          onSubmit={formik.handleSubmit}
+        >
+          <AuthTextField
+            fullWidth
+            id="username"
+            name="username"
+            label="Username"
+            value={formik.values.username}
+            onChange={formik.handleChange}
+            error={formik.touched.username && Boolean(formik.errors.username)}
+          />
+          <AuthTextField
+            fullWidth
+            id="password"
+            name="password"
+            label="Password"
+            type="password"
+            value={formik.values.password}
+            onChange={formik.handleChange}
+            error={formik.touched.password && Boolean(formik.errors.password)}
+          />
+          <AuthButton
+            color="primary"
+            variant="contained"
+            fullWidth
+            type="submit"
+          >
+            Submit
+          </AuthButton>
+        </form>
+        <Snackbar
+          open={isOpen}
+          autoHideDuration={6000}
+          onClose={handleClose}
+          message={responseMessage}
+        />
+      </div>
+    </>
+  );
+};
+
+export default LoginForm;
