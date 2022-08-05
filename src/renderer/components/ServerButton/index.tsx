9479b98675b7a85ed00c9c4dccc9d59322373fd0
dSandley20 <danielsandley2@gmail.com>
2022-08-05 10:38:55 -0400

    Server button component for server list menu

--- src/renderer/components/ServerButton/index.tsx
+++ src/renderer/components/ServerButton/index.tsx
@@ -1,5 +1,20 @@
-const ServerButton = (props) => {
-  return <></>;
+import IServerButton from 'interfaces/server/IServerButton';
+import { Avatar } from '@mui/material';
+
+const ServerButton = (props: IServerButton) => {
+  const { serverName, serverInitials, serverImage, onClick } = props;
+  return (
+    <>
+      {serverImage != null && (
+        <Avatar alt={serverName} src={serverImage} onClick={onClick} />
+      )}
+      {serverImage == null && (
+        <Avatar alt={serverName} onClick={onClick}>
+          {serverInitials}
+        </Avatar>
+      )}
+    </>
+  );
 };
 
 export default ServerButton;
