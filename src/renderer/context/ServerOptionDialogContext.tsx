10adeff6ad8cbd73411fa675a061579b7305969e
dSandley20 <danielsandley2@gmail.com>
2022-08-10 11:09:56 -0400

    add context variables for parent dialog

--- src/renderer/context/ServerOptionDialogContext.tsx
+++ src/renderer/context/ServerOptionDialogContext.tsx
@@ -1,23 +1,32 @@
 import { createContext, useContext, useState } from 'react';
 
 export interface IServerOptionDialogContext {
+  isOpen: boolean;
   isCreate: boolean;
   isJoin: boolean;
+  updateIsOpen: (data: boolean) => void;
   updateIsCreate: (data: boolean) => void;
   updateIsJoin: (data: boolean) => void;
 }
 
 const ServerOptionsDialogContext = createContext<IServerOptionDialogContext>({
+  isOpen: false,
   isCreate: false,
   isJoin: false,
+  updateIsOpen(data) {},
   updateIsCreate(data) {},
   updateIsJoin(data) {},
 });
 export default ServerOptionsDialogContext;
 
 const ServerOptionsDialogProvider = ({ children }: any) => {
+  const [isOpen, setIsOpen] = useState(false);
   const [isCreate, setIsCreate] = useState(false);
   const [isJoin, setIsJoin] = useState(false);
+
+  const updateIsOpen = (data: boolean) => {
+    setIsOpen(data);
+  };
   const updateIsCreate = (data: boolean) => {
     setIsCreate(data);
   };
@@ -27,7 +36,14 @@ const ServerOptionsDialogProvider = ({ children }: any) => {
 
   return (
     <ServerOptionsDialogContext.Provider
-      value={{ isCreate, isJoin, updateIsCreate, updateIsJoin }}
+      value={{
+        isOpen,
+        isCreate,
+        isJoin,
+        updateIsOpen,
+        updateIsCreate,
+        updateIsJoin,
+      }}
     >
       {children}
     </ServerOptionsDialogContext.Provider>
